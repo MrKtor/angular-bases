dcaf111679b31d81d43e41b3fb77393b93bdf4ed
Héctor Rivas <hectorrivasexposito@gmail.com>
2024-09-30 11:43:54 +0200

    build

--- src/app/dbz/pages/main-page.component.ts
+++ src/app/dbz/pages/main-page.component.ts
@@ -9,10 +9,23 @@ import { DbzService } from '../services/dbz.service';
 
 export class MainPageComponent {
 
-  // #dbzService = inject(DbzService); Es un ejemplo de inyección de dependencias
+  #dbzService = inject(DbzService);
+  //Es un ejemplo de inyección de dependencias
   //De la misma forma que es el constructor
 
-  constructor(public dbzService: DbzService) {
+  // constructor(private dbzService: DbzService) {}
 
+
+  get characters(): Character[] {
+    return [...this.#dbzService.characters];
   }
+
+  onDeleteCharacter(id: string): void {
+    this.#dbzService.deleteCharacterById(id);
+  }
+
+  onNewCharacter( character: Character): void {
+    this.#dbzService.onNewCharacter( character );
+  }
+
 }
